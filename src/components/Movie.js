796aa4c091a8a06d0c1b157b0d886e7926e7cb36
AnilP. <anl.pirincoglu@gmail.com>
2024-01-07 14:59:35 +0300

    add delete movie function

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { useParams, useHistory } from "react-router-dom";
+import { deleteMovie } from "../actions/movieActions";
 
 const Movie = (props) => {
   const { id } = useParams();
@@ -8,6 +9,13 @@ const Movie = (props) => {
 
   const movies = useSelector((store) => store.movies);
   const movie = movies.find((movie) => movie.id === Number(id));
+
+  const dispatch = useDispatch();
+
+  const deleteHandler = () => {
+    dispatch(deleteMovie(id));
+    push("/movies");
+  };
 
   return (
     <div className="bg-white rounded-md shadow flex-1">
@@ -37,7 +45,11 @@ const Movie = (props) => {
         </div>
       </div>
       <div className="px-5 py-3 border-t border-zinc-200 flex justify-end gap-2">
-        <button type="button" className="myButton bg-red-600 hover:bg-red-500">
+        <button
+          onClick={deleteHandler}
+          type="button"
+          className="myButton bg-red-600 hover:bg-red-500"
+        >
           Sil
         </button>
         <button className="myButton bg-blue-600 hover:bg-blue-500 ">
